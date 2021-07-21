6b776765f2211912d854b9cbce28d7ce6cc4c272
Niels Slo <nielssloesen@hotmail.com>
2021-07-21 11:01:39 +0200

    Updated Waypoints animations for footer.php

--- wp-content/themes/inkoppers/footer.php
+++ wp-content/themes/inkoppers/footer.php
@@ -19,7 +19,7 @@ $footerCopyright = get_field("footer_copyright", "option");
 
 <footer class="bgc-gray-5 <?php if ( is_page( 1 ) ): ?><?php echo "bgc-dark"; ?><?php endif; ?> pt-lg-5">
     <!-- Mobile -->
-    <div class="container-fluid top-footer-mobile d-lg-none">
+    <div class="container-fluid top-footer-mobile d-lg-none rise-animation">
         <div class="row">
             <div class="col-3">
                 <img src="/future-vastgoed/wp-content/uploads/2021/07/footer-logo-small.svg" alt="" class="pt-2" />
@@ -43,47 +43,49 @@ $footerCopyright = get_field("footer_copyright", "option");
         </div>
     </div>
     <!-- Desktop -->
-    <div class="container-fluid top-footer-desktop py-lg-5 d-none d-lg-block">
-        <div class="row my-lg-5">
-            <div class="col-lg-1 offset-lg-1">
-                <img src="/future-vastgoed/wp-content/uploads/2021/07/footer-logo.svg" alt="" class="pt-3 mw-100" />
-            </div>
-            <div class="col-lg-3">
-                <?php if ($footerAddress1): ?>
-                    <address class="footer-address">
-                        <?php echo $footerAddress1; ?>
-                    </address>
-                <?php endif; ?>
-            </div>
-            <div class="col-lg-3">
-                <?php if ($footerAddress1): ?>
-                    <address>
-                        <?php echo $footerAddress2; ?>
-                    </address>
-                <?php endif; ?>
-            </div>
-            <div class="col-lg-2 offset-lg-1 d-flex flex-column">
-                <?php if ($footerLink1): ?>
-                    <a href="<?php echo $footerLink1["url"]; ?>" target="<?php echo $footerLink1["target"]; ?>" class="fs-2030 clr-gray-4 fw-extra-light"
-                        ><?php echo $footerLink1["title"];?></a
-                    >
-                <?php endif; ?>
-                <?php if ($footerLink2): ?>
-                    <a href="<?php echo $footerLink2["url"]; ?>" target="<?php echo $footerLink2["target"]; ?>" class="fs-2030 clr-gray-4 fw-extra-light"
-                        ><?php echo $footerLink2["title"];?></a
-                    >
-                <?php endif; ?>
-                <?php if ($footerLink3): ?>
-                    <a href="<?php echo $footerLink3["url"]; ?>" target="<?php echo $footerLink3["target"]; ?>" class="fs-2030 clr-gray-4 fw-extra-light"
-                        ><?php echo $footerLink3["title"];?></a
-                    >
-                <?php endif; ?>
+    <div class="waypoint-animation">
+        <div class="container-fluid top-footer-desktop py-lg-5 d-none d-lg-block rise-animation">
+            <div class="row my-lg-5">
+                <div class="col-lg-1 offset-lg-1">
+                    <img src="/future-vastgoed/wp-content/uploads/2021/07/footer-logo.svg" alt="" class="pt-3 mw-100" />
+                </div>
+                <div class="col-lg-3">
+                    <?php if ($footerAddress1): ?>
+                        <address class="footer-address">
+                            <?php echo $footerAddress1; ?>
+                        </address>
+                    <?php endif; ?>
+                </div>
+                <div class="col-lg-3">
+                    <?php if ($footerAddress1): ?>
+                        <address>
+                            <?php echo $footerAddress2; ?>
+                        </address>
+                    <?php endif; ?>
+                </div>
+                <div class="col-lg-2 offset-lg-1 d-flex flex-column">
+                    <?php if ($footerLink1): ?>
+                        <a href="<?php echo $footerLink1["url"]; ?>" target="<?php echo $footerLink1["target"]; ?>" class="fs-2030 clr-gray-4 fw-extra-light"
+                            ><?php echo $footerLink1["title"];?></a
+                        >
+                    <?php endif; ?>
+                    <?php if ($footerLink2): ?>
+                        <a href="<?php echo $footerLink2["url"]; ?>" target="<?php echo $footerLink2["target"]; ?>" class="fs-2030 clr-gray-4 fw-extra-light"
+                            ><?php echo $footerLink2["title"];?></a
+                        >
+                    <?php endif; ?>
+                    <?php if ($footerLink3): ?>
+                        <a href="<?php echo $footerLink3["url"]; ?>" target="<?php echo $footerLink3["target"]; ?>" class="fs-2030 clr-gray-4 fw-extra-light"
+                            ><?php echo $footerLink3["title"];?></a
+                        >
+                    <?php endif; ?>
+                </div>
             </div>
         </div>
     </div>
 
     <!-- Mobile bottom footer -->
-    <div class="container-fluid bottom-footer-mobile d-lg-none">
+    <div class="container-fluid bottom-footer-mobile d-lg-none rise-animation">
         <div class="row my-4 mx-0">
             <?php if ($footerTerms): ?>
                 <a href="<?php echo $footerTerms["url"]; ?>" target="<?php echo $footerTerms["target"]; ?>" class="fs-1520 clr-gray-4 fw-extra-light mr-4">
@@ -113,7 +115,7 @@ $footerCopyright = get_field("footer_copyright", "option");
 
     <!-- Desktop bottom footer -->
     <div
-        class="container-fluid bottom-footer-desktop py-lg-5 d-none d-lg-block"
+        class="container-fluid bottom-footer-desktop py-lg-5 d-none d-lg-block rise-animation"
     >
         <div class="row">
             <div class="col-lg-4 offset-lg-1 d-flex justify-content-between">
